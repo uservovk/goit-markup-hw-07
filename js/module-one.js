a24a961ce26bab3576197d847e3cf3f4a8d8ade9
Oleksandr  Vovk <vovk.sasha@outlook.com>
2022-09-11 21:39:02 +0700

    added script

---
+++ js/module-one.js
@@ -0,0 +1,22 @@
+const totalPrice = 108;
+const age = 36;
+const userName = 'Alex';
+const message = 'Hello World';
+const isOpen = true;
+const shouldConfirm = false;
+
+let height = 123;
+height = 54;
+
+console.log(5)
+console.log(age)
+console.log(message)
+console.log('My age is:', age, 'years old!')
+console.log('My name is:', userName, '.')
+
+const type = typeof 'hi there';
+console.log(type);
+const weight = typeof 156;
+console.log(weight);
+const isClosed = typeof false;
+console.log(isClosed);
