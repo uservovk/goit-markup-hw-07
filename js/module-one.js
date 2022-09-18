e5be298afb8d0a39f4c4567904917b52b649c590
Oleksandr  Vovk <vovk.sasha@outlook.com>
2022-09-18 16:13:38 +0700

    added math

--- js/module-one.js
+++ js/module-one.js
@@ -1,45 +1,71 @@
-const totalPrice = 108;
-const age = 36;
-const userName = 'Alex';
-const message = 'Hello World';
-const isOpen = true;
-const shouldConfirm = false;
+// const totalPrice = 108;
+// const age = 36;
+// const userName = 'Alex';
+// const message = 'Hello World';
+// const isOpen = true;
+// const shouldConfirm = false;
 
-let height = 123;
-height = 54;
+// let height = 123;
+// height = 54;
 
-console.log(5)
-console.log(age)
-console.log(message)
-console.log('My age is:', age, 'years old!')
-console.log('My name is:', userName, '.')
+// console.log(5)
+// console.log(age)
+// console.log(message)
+// console.log('My age is:', age, 'years old!')
+// console.log('My name is:', userName, '.')
 
-const type = typeof 'hi there';
-console.log(type);
-const weight = typeof 156;
-console.log(weight);
-const isClosed = typeof false;
-console.log(isClosed);
+// const type = typeof 'hi there';
+// console.log(type);
+// const weight = typeof 156;
+// console.log(weight);
+// const isClosed = typeof false;
+// console.log(isClosed);
 
-// alert('dnvfjnv')
-// console.log('Say:', message)
+// // alert('dnvfjnv')
+// // console.log('Say:', message)
 
-// const shouldRenew = confirm('Would like to renew subscription?');
-// console.log(shouldRenew);
+// // const shouldRenew = confirm('Would like to renew subscription?');
+// // console.log(shouldRenew);
 
-// let quantity = prompt('Write the number of pessengers');
-// quantity = Number(quantity);
-// console.log(quantity);
-// console.log(typeof quantity);
+// // let quantity = prompt('Write the number of pessengers');
+// // quantity = Number(quantity);
+// // console.log(quantity);
+// // console.log(typeof quantity);
 
-let elementWidth = "50px";
-const result = Number.parseInt(elementWidth);
-console.log(typeof result);
+// let elementWidth = "50px";
+// const result = Number.parseInt(elementWidth);
+// console.log(typeof result);
 
-let elementHeight = '200.11px';
-elementHeight = Number.parseFloat(elementHeight); 
-console.log(elementHeight);
+// let elementHeight = '200.11px';
+// elementHeight = Number.parseFloat(elementHeight);
+// console.log(elementHeight);
 
-let salary = 1200.65748;
-salary = salary.toFixed(2);
-console.log(salary);
+// let salary = 1200.65748;
+// salary = salary.toFixed(2);
+// console.log(salary);
+
+// const value = 'today is friday';
+// console.log(Number(value));
+// console.log(typeof Number(value));
+
+
+// console.log(Math.pow(2,3));
+// console.log(2**3);
+
+// let prac = prompt('give me a number');
+// prac = Number(prac);
+// console.log(prac);
+// let power = prompt('give me a power');
+// power = Number(power);
+// console.log(power);
+// console.log(prac ** power);
+
+// const max = 100;
+// const min = 1;
+// const result = Math.round(Math.random() * (max - min) + min);
+// console.log(result);
+// const message = 'Q: how many symbols in this stroke?';
+// console.log(message.length);
+const firstName = 'Alex';
+const lastName = 'Vovk';
+console.log(firstName + ' ' + lastName);
