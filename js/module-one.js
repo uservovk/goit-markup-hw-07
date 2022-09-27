79920136dfdfd713e7cbd3c7d22e886b4f352949
Oleksandr  Vovk <vovk.sasha@outlook.com>
2022-09-27 20:54:04 +0700

    if-else

--- js/module-one.js
+++ js/module-one.js
@@ -156,3 +156,12 @@ function checkDegree(degree) {
     }
     return message;
 }
+function checkStorage(available, ordered) {
+  let message;
+if(ordered>available){
+  message='Not enough goods in stock!';
+} else {
+  message='Order is processed, our manager will contact you.';
+}
+  return message;
+}
